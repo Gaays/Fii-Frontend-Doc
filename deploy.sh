dd74e553815cee3aca69eb9723efb12a54ee5efc
shujiapeng <shujiapeng@outlook.com>
2021-09-23 16:14:32 +0800

    update github page

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,19 @@
 set -e
 
 # 生成静态文件
-npm run build
+npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
-
-#创建.nojekyll 防止Github Pages build错误
-touch .nojekyll
 
 git init
 git add -A
 git commit -m 'deploy'
 
-git push -f "https://${access_token}@github.com/PanJiaChen/vue-element-admin-site.git" master:gh-pages
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:Gaays/Fii-Frontend-Doc.git master:gh-pages
 
 cd -
